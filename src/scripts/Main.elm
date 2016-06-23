a61b40ed92a6d39968e740991615593422f68925
Bryan Brick <wbrick@towson.edu>
2016-06-22 22:03:57 -0400

    define cell manipulation and grid update functions

--- src/scripts/Main.elm
+++ src/scripts/Main.elm
@@ -22,3 +22,48 @@ type alias Position =
 
 type alias Row = Array CellState
 type alias Grid = Array Array CellState
+
+createGrid : Int -> Int -> Grid
+createGrid width height =
+  Array.initialize height (\_ -> Array.initialize width (\_ -> Dead ) )
+
+getCellValue : Grid -> Position -> CellState
+getCellValue grid {row, col} =
+  case Maybe.andThen (Array.get col grid) (Array.get row) of
+      Just val -> val
+      Nothing -> Dead
+
+getNeighborPositions : Position -> List Position
+getNeighborPositions {row, col} =
+  List.foldl
+    (\posn posns -> List.append
+       ( (\x -> List.map (\y -> {row = x, col = y} ) [col-1 .. col+1] ) posn)
+       posns
+    )
+    []
+    [row-1 .. row+1]
+
+getNeighborValues : Grid -> Position -> List CellState
+getNeighborValues grid position =
+  List.map (getCellValue grid) (getNeighborPositions position )
+
+getTotalLivingNeighbors : Grid -> Position -> Int
+getTotalLivingNeighbors grid position =
+  List.length
+    List.filter (\cell -> cell == Alive) (getNeighborValues grid position) 
+
+updateCell : Grid -> Position -> CellState -> Grid
+updateCell grid {row, col} cellState =
+  Array.set
+    row
+    Array.set col cellState (Array.get row grid)
+    grid
+
+updateRow : Row -> Row
+updateRow row =
+  Array.map updateCell row
+
+updateGrid : Grid -> Grid
+updateGrid grid =
+  Array.map updateRow grid
+
